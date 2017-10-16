882229d51320182762409e1c6e5f750e13632727
Igor Danich <i.danich@besk.com>
2017-10-16 10:39:16 +0300

    fix header

--- DCFoundation-iOS/DCFoundation.h
+++ DCFoundation-iOS/DCFoundation.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Igor Danich. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for DCFoundation-iOS.
 FOUNDATION_EXPORT double DCFoundation_iOSVersionNumber;
