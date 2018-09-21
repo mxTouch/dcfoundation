b3cd20aa0b3c00d6624932e5f010b4a85c8d33e0
Igor Danich <dclife2009@gmail.com>
2018-09-21 11:48:03 +0300

    add fixes

--- DCFoundation-iOS/DCFoundation_iOS.h
+++ DCFoundation-iOS/DCFoundation_iOS.h
@@ -10,6 +10,8 @@ FOUNDATION_EXPORT double DCFoundation_iOSVersionNumber;
 //! Project version string for DCFoundation_iOS.
 FOUNDATION_EXPORT const unsigned char DCFoundation_iOSVersionString[];
 
+@import Foundation;
+
 // In this header, you should import all the public headers of your framework using statements like #import <DCFoundation_iOS/PublicHeader.h>
 
 
