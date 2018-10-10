4f4e3903b5944a9e18a4f4b577c1cb5665716eca
Igor Danich <igor.danich@agrello.org>
2018-10-10 20:49:10 +0300

    fixes string extension

--- DCFoundation/Extensions/String.swift
+++ DCFoundation/Extensions/String.swift
@@ -15,23 +15,16 @@ public extension String {
 
 public extension String {
     
-    subscript (i: Int) -> String {
+    public subscript (i: Int) -> String {
         return self[i ..< i + 1]
     }
     
-    func substring(fromIndex: Int) -> String {
+    public func substring(fromIndex: Int) -> String {
         return self[min(fromIndex, length) ..< length]
     }
     
-    func substring(toIndex: Int) -> String {
+    public func substring(toIndex: Int) -> String {
         return self[0 ..< max(0, toIndex)]
-    }
-    
-    subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)), upper: min(length, max(0, r.upperBound))))
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return String(self[start ..< end])
     }
     
 }
